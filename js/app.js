58a89552cf248de29723c7c2c3a3f55483e80496
James Le <le_j6@denison.edu>
2017-08-11 17:19:59 -0700

    Create superclass for GameObjects and logic class GameController

--- js/app.js
+++ js/app.js
@@ -1,90 +1,172 @@
-// Enemies our player must avoid
-var Enemy = function (x, y, velocity) {
-    // shared enemy sprite, uses prewritten helper
-    this.sprite = 'images/enemy-bug.png';
+/**
+ *  GameObject Superclass
+ *  - shared functions and properties for moving critters
+ */
+var GameObject = function (xPos, yPos, velocity, xAxis, yAxis, spriteURL) {
+    // load sprite
+    this.sprite = spriteURL;
 
-    // enemy properties
-    this.start_x = x;       // initial x,y
-    this.start_y = y;
-    this.x = x;             // updated x,y
-    this.y = y;
-    this.speed = velocity;  // enemy speed
+    // starting properties
+    this.start_x = xPos;        // initial x,y
+    this.start_y = yPos;
+    this.x = xPos;              // updated x,y
+    this.y = yPos;
+    this.start_horiz = xAxis;   // initial movement axes
+    this.start_vert = yAxis;
+    this.horiz = xAxis;         // updated movement axes
+    this.vert = yAxis;
+    this.speed = velocity;      // object speed
 };
 
-// Enemy behavior in update loop
-Enemy.prototype.update = function(deltaTime) {
+// Draw the object on the screen using canvas context (ctx) in engine.js
+GameObject.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
-    // check for player "collision" (close x,y locs)
+// Move method to call in subclass updates
+GameObject.prototype.move = function (deltaTime) {
+    // update location this frame
+    if (this instanceof Player) {
+        // player movement decoupled from deltaTime (causes no-show)
+        this.x += this.horiz * this.speed;
+        this.y += this.vert * this.speed;
+    } else {
+        // standard movement along axes at speed smoothed by time
+        this.x += this.horiz * this.speed * deltaTime;
+        this.y += this.vert * this.speed * deltaTime;
+    }
+};
+
+// Reset player position and movement
+GameObject.prototype.respawn = function () {
+    this.x = this.start_x, this.y = this.start_y;
+    this.horiz = this.start_horiz, this.vert = this.start_vert;
+};
+
+// Check for "collision" (close x,y position)
+GameObject.prototype.collidedWith = function(obj) {
+    // set detection radius
     var collisionSensitivity = 25;
-    if (Math.abs(this.x - player.x) < collisionSensitivity) {
-        if (Math.abs(this.y - player.y) < collisionSensitivity) {
-            // player killed - game over
-            player.respawn();
+
+    // see if other object's x and y distance falls within radius
+    if (Math.abs(this.x - obj.x) < collisionSensitivity) {
+        if (Math.abs(this.y - obj.y) < collisionSensitivity) {
+            return true;    // collision
         }
     }
+    return false;   // no collision
+};
 
-    // move forward
-    this.x += this.speed * deltaTime;
-
-    // out-of-bounds respawn
-    if (this.x > ctx.canvas.width) {
-        this.x = this.start_x;
+// Check for movement out of bounds
+GameObject.prototype.outOfBounds = function () {
+    /*  determine screen bounds based on this object's class
+     *  (left: x0, right: x1, up: y0, down: y1)
+     */
+    if (this instanceof Player) {
+        // boundary calibrated to player movement and tile locations
+        var x0 = -50, x1 = ctx.canvas.width-50;
+        var y0 = -10, y1 = ctx.canvas.height-170;
+    } else if (this instanceof Enemy) {
+        // boundary calibrated to enemy movement and off-screen spawn
+        var x0 = -9000, x1 = ctx.canvas.width;
+        var y0 = -9000, y1 = ctx.canvas.height;
+    } else {
+        // default boundary
+        var x0 = 0, x1 = ctx.canvas.width;
+        var y0 = 0, y1 = ctx.canvas.height;
     }
+
+    // see if this object falls outside boundaries set above
+    if (this.x < x0 || this.x > x1 || this.y < y0 || this.y > y1) {
+        return true;    // out of bounds
+    }
+    return false;   // within bounds
 };
 
-// Draw the enemy on the screen using canvas context (ctx) in engine.js
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+/**
+ *  Enemy Subclass : GameObject
+ */
+var Enemy = function (xPos, yPos, velocity) {
+    // shared enemy sprite, uses resources.js
+    var spriteURL = 'images/enemy-bug.png';
+
+    // enemy movement direction (move right)
+    var xAxis=1, yAxis=0;
+
+    // instantiate as pseudoclassical subclass
+    GameObject.call(this, xPos, yPos, velocity, xAxis, yAxis, spriteURL);
+};
+// delegate failed method lookups to superclass
+Enemy.prototype = Object.create (GameObject.prototype);
+// set Enemy as the .constructor instead of GameObject
+Enemy.prototype.constructor = Enemy;
+
+// Behavior loop
+Enemy.prototype.update = function(deltaTime) {
+    // basic movement
+    this.move (deltaTime);
+
+    // check for player collision
+    this.collidedWith(player) ? player.die() : 0;
+
+    // respawn if enemy moves outside screen
+    this.outOfBounds() ? this.respawn() : 0;
 };
 
 
-// Main player moved by input
-var Player = function (x, y, velocity) {
-    // load player sprite
-    this.sprite = 'images/char-boy.png'
-    /* EXTRA: let player choose sprite */
-
-    // player instance properties
-    this.start_x = x;       // initial x,y
-    this.start_y = y;
-    this.x = x;             // updated x,y
-    this.y = y;
-    this.horiz = 0;         // player movement input axes
-    this.vert = 0;
-    this.speed = velocity;  // player speed
+/**
+ *  Main Player Subclass : GameObject
+ */
+var Player = function (x, y, velocity, spriteURL) {
+    GameObject.call (this, x, y, velocity, 0, 0, spriteURL)
 };
+// delegate methods up the chain to GameObject
+Player.prototype = Object.create (GameObject.prototype);
+// reset the constructor to the Player class, not GameObject
+Player.prototype.constructor = Player;
 
-// Player behavior in update loop
-Player.prototype.update = function (deltaTime) {
-
+// Player behavior loop
+Player.prototype.update = function(deltaTime) {
     // update location
-    this.x += (this.horiz * this.speed); // * deltaTime); // /!\ dt == NaN /!\
-    this.y += (this.vert * this.speed);
+    this.move (deltaTime);
 
-    // screen dimensions for player bounds
-    screenX = ctx.canvas.width-50;
-    screenY = ctx.canvas.height-170;
+    // end zone win condition check
+    this.reachedGoal() ? this.win() : 0;
 
     // out-of-bounds respawn
-    if (this.x > screenX || this.x < -50 || this.y < -10 || this.y > screenY){
-        this.respawn();
-    }
+    this.outOfBounds() ? this.die() : 0;
+
+    // show the current score
+    console.log ("Score: "+gameController.score);
 };
 
-// Render player sprite using canvas context (ctx) in engine.js
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Check if player made it to the top of the screen
+Player.prototype.reachedGoal = function () {
+    if (this.y < 0) {
+        return true;
+    } return false;
+}
 
-// reset player position
-Player.prototype.respawn = function () {
-    this.x = this.start_x, this.y = this.start_y;
-};
+// Declare victory
+Player.prototype.win = function () {
+    console.log ("You win!");
+    gameController.score += 10;
+    this.respawn();
+}
 
+// Admit defeat
+Player.prototype.die = function () {
+    console.log ("You didn't make it!");
+    gameController.score = 0;
+    this.respawn();
+}
+
+// Take keyboard input from event listener added by GameController
 Player.prototype.handleInput = function (key) {
-    // reset input axes in case no key is pressed
-    this.horiz = 0;
-    this.vert = 0;
+    // reset input axes on new keypress (avoids diagonal)
+    this.horiz = this.start_horiz;
+    this.vert = this.start_vert;
 
     // set axes according to key press
     switch (key) {
@@ -104,26 +186,59 @@ Player.prototype.handleInput = function (key) {
 };
 
 
-// Instantiate enemy objects in an array
-var allEnemies = new Array();
-var initX=-101, initY=55;  // x, y loc references
-for (var i = 0; i < 3; i++) {
-    // use engine.js render() row and col dimensions to set x, y
-    allEnemies.push(new Enemy(initX*(i+1), (i*83)+initY, 150));
-}
+/**
+ *  Game Logic & Gamewide Properties
+ */
+var GameController = function () {
+    // properties
+    this.score = 0;
+
+    // listen for keypress and send to Player's handleInput(key)
+    document.addEventListener('keyup', function(e) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
+        };
+        player.handleInput(allowedKeys[e.keyCode]);
+    });
+
+};
 
 // Instantiate the player object
-var player = new Player (300, 390, 2);
+GameController.prototype.spawnPlayer = function (spriteURL) {
+    var player = new Player (300, 390, 3, spriteURL);
+    return player;
+};
 
+// Instantiate enemy objects in an array
+GameController.prototype.spawnEnemies = function (enemyCount) {
+    var enemies = new Array();
+    var randomSpeed;
+    var initX=-101, initY=55;  // x, y loc references
+    for (var i = 0; i < Math.floor(enemyCount/2); i++) {
+        // use engine.js render() row and col dimensions to set x, y
+        randomSpeed = Math.random() * 300 + 80;
+        enemies.push(new Enemy(initX*(i+1), (i*83)+initY, randomSpeed));
+        enemies.push(new Enemy(initX*(i+3), (i*83)+initY, randomSpeed));
+    }
+    return enemies;
+};
 
-// Listen for keypress and send key to Player.handleInput()
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+// choose player sprite based on URL param passed from index.html
+var charName = window.location.search.substring(1);
+if (charName == 'zq123') {
+    // why aren't other images working?
+    /*
+    Failed to execute 'drawImage' on 'CanvasRenderingContext2D': The provided value is not of type '(HTMLImageElement or HTMLVideoElement or HTMLCanvasElement or ImageBitmap)'
+    */
+    var spriteURL = 'images/char-princess-girl.png';
+} else {
+    var spriteURL = 'images/char-boy.png';
+}
 
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+// start game through game controller
+var gameController = new GameController();
+var player = gameController.spawnPlayer(spriteURL);
+var allEnemies = gameController.spawnEnemies(6);
--- js/engine.js
+++ js/engine.js
@@ -91,10 +91,12 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
-        });
-        player.update();
+        if (typeof allEnemies != "undefined" && typeof player != "undefined") {
+            allEnemies.forEach(function(enemy) {
+                enemy.update(dt);
+            });
+            player.update();
+        }
     }
 
     /* This function initially draws the "game level", it will then call
@@ -147,11 +149,13 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
+        if (typeof allEnemies != "undefined" && typeof player != "undefined") {
+            allEnemies.forEach(function(enemy) {
+                enemy.render();
+            });
 
-        player.render();
+            player.render();
+        }
     }
 
     /* This function does nothing but it could have been a good place to
