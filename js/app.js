810499d8658ae491a8bd59842187f1fee87cd3ba
James Le <le_j6@denison.edu>
2017-08-11 17:16:19 -0700

    Sketch Enemy and Player class functions and update methods

--- js/app.js
+++ js/app.js
@@ -1,30 +1,72 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function (x, y, velocity) {
+    // shared enemy sprite, uses prewritten helper
     this.sprite = 'images/enemy-bug.png';
+
+    // enemy instance variables
+    this.x = x;
+    this.y = y;
+    this.speed = velocity;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+// Enemy behavior in update loop
+Enemy.prototype.update = function(deltaTime) {
+    // check for player "collision" (same tile occupied)
+    if (this.x == player.x && this.y == player.y) {
+        // player killed - game over
+        console.log ("OUCH! Enemy hit you!!")
+    }
+    // move forward
+    this.x += this.speed * deltaTime;
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen using canvas context (ctx) in engine.js
 Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    /* if out of bounds, respawn */
+};
+
+
+// Main player moved by input
+var Player = function (x, y, velocity) {
+    // load player sprite
+    this.sprite = 'images/player.png'
+    /* EXTRA: let player choose sprite */
+
+    // player instance properties
+    this.x = x;
+    this.y = y;
+    this.speed = velocity;
+};
+
+// Player behavior in update loop
+Player.prototype.update = function (deltaTime) {
+    this.x += (this.speed * deltaTime);
+    /* check for out of bounds */
+};
+
+// Render player sprite using canvas context (ctx) in engine.js
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+Player.prototype.handleInput = function (key) {
 
+    switch (key) {
+        case 'up':
+            this.y++;
+            break;
+        case 'down':
+            this.y--;
+            break;
+        case 'left':
+            this.x++;
+            break;
+        case 'right':
+            this.x--;
+            break;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -32,8 +74,7 @@ Enemy.prototype.render = function() {
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for keypress and send key to Player.handleInput()
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
